105514b813b47c4cd722a3643598d595ad517f29
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-02-12 13:57:51 -0500

    D

--- archaeoMaps.js
+++ archaeoMaps.js
@@ -4,10 +4,11 @@
 function initMap() {
 // The location of my old place in Stony
 const Stony = { lat: 52.05640827732168, lng: -0.8504520908370468 }; // Home Location 52.05640827732168, -0.8504520908370468
-// The map, centered at Stony Stratfor
+
 const map = new google.maps.Map(document.getElementById("contactMap"), {
 zoom: 13,
 center: Stony, // centres on Lakehead
+mapTypeId: "terrain",
 });
 // The marker, positioned at Lakehead
 const marker = new google.maps.Marker({
